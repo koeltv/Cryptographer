ffc5c07dd8cd20b483e10ea7e1e3fa2846d19ea0
koeltv <valentin.koeltgen@utt.fr>
2020-10-29 15:23:24 +0100

    Premier commit d'essai

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int k=0;
+    char tempchar, input[101] = ""; //limite à 100 caractères (avec '\0')
+    printf("Saisir une chaine de caracteres : ");
+    scanf("%c", &tempchar);
+    while (tempchar > 31 && tempchar < 127){ //On vérifie que les caractères soit des caractères visbles (voir table ASCII)
+        input[k] = tempchar;
+        scanf("%c", &tempchar);
+        k++;
+    }
+    printf("La chaine de caracteres encodee vaut : %s", input);
     return 0;
 }
