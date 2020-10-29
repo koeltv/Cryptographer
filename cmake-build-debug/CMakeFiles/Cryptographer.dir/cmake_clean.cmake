0c147406ad8c3d6bc8b4261aaebf3e772e8e467f
koeltv <valentin.koeltgen@utt.fr>
2020-10-29 15:09:23 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Cryptographer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cryptographer.dir/main.c.obj"
+  "Cryptographer.exe"
+  "Cryptographer.exe.manifest"
+  "Cryptographer.lib"
+  "Cryptographer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cryptographer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
