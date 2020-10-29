3259df347d50951b1d4b80d069979c911748e7e0
koeltv <valentin.koeltgen@utt.fr>
2020-10-29 20:43:14 +0100

    -Création des fonctions readFile() pour la lecture de fichier, CharPermutation() correspondant à la 1ère fonction demandée et CharPermutationReverse() permettant d'inverser la précédante, -Ajout de commentaire pour rendre le programme plus clair.

--- main.c
+++ main.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
+#define EncryptionKey "NF05 Rules" //Clé d'encodage, on pourra la demander plus tard
 
-int main() {
+//Fonction permettant de lire un fichier externe en indiquant le lien relatif et qui renvoie la taille du fichier
+unsigned int readFile(char file[]){
     FILE *fp;
-    fp = fopen("../test.txt", "r"); //On accède au fichier à encoder
+    char link[] = "../test.txt";
+    //printf("Entrer le lien du fichier relatif au programme (ex: \"../test.txt\") : ");
+    //scanf("%s", link);
+    fp = fopen(link, "rb"); // On accède au fichier à encoder
 
     fseek(fp, 0L, SEEK_END); // Recherche de la fin du fichier
     unsigned int size = ftell(fp); // Stockage de la taille
     rewind(fp); // Replacement du pointeur au début du fichier
 
-    char buffer;
-    for (int i = 0; i < size; i++) {
-        fscanf(fp, "%c", &buffer);
-        if (buffer == 10) i++;
-        printf("%c", buffer);
-    }
+    for (int i = 0; i < size; i++) fscanf_s(fp, "%c", &file[i]);
     fclose(fp);
+    return size;
+}
+
+//Permutation des caractères (on les décale de la valeur de key puis on applique modulo 255)
+char CharPermutation(char car, int key){
+    return (char)(((int)car+key)%255);
+}
+//Fonction inverse de celle ci-dessus
+char CharPermutationReverse(char car, int key){
+    return (char)((int)car - key%255);
+}
+
+int main() {
+    int key = 0, i=0;
+    //Création d'une valeur utilisée pour la permutation des charactères dépendante d'EncryptionKey
+    while (EncryptionKey[i] != '\0'){
+        key += EncryptionKey[i];
+        i++;
+    }
+    printf("Key value : %d\n", key);
+    char file[1000]; //TODO Mettre en place l'allocation dynamique de mémoire
+    unsigned int size = readFile(file);
+
+    //Etape 1
+    for (int j = 0; j < size; j++) file[j] = CharPermutation(file[j], key);
+
+    //Affichage du fichier après encodage
+    printf("\n======================================\n");
+    for (i = 0; i < size; i++) printf("%c", file[i]);
+    printf("\n======================================");
 
     return 0;
 }
